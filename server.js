7a111be7ecd9afa0d56735e4e541bb317b039fd8
0903chris <42923822+0903chris@users.noreply.github.com>
2018-11-27 15:33:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/',function(req,res) {
 		res.redirect('/login');
 	} else {
 		res.status(200);
-		res.render('secrets',{name:req.session.username});
+		res.render('restaurants',{name:req.session.username});
 	}
 });
 
@@ -57,29 +57,4 @@ app.get('/logout',function(req,res) {
 	req.session = null;
 	res.redirect('/');
 });
-var server = http.createServer(function (req, res) {
-  var parsedURL = url.parse(req.url,true);
-  
-  if (parsedURL.pathname == '/fileupload' && 
-      req.method.toLowerCase() == "post") {
-    // parse a file upload
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var filename = files.filetoupload.path;
-      console.log("filename = " + filename);
-      fs.readFile(filename, function(err,data) {
-         var base64 = new Buffer(data).toString('base64');
-         res.writeHead(200,{"Content-Type": "text/plain"});
-         res.write('File uploaded: (Base64)\n');         
-         res.end(base64);
-      })
-    });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    res.end();
-  }
-app.listen(process.env.PORT || 8099);
+
