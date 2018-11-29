c85edf221aecbc72404d9146747c0ad2f647ed13
0903chris <42923822+0903chris@users.noreply.github.com>
2018-11-29 19:03:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -84,9 +84,7 @@ app.get('/gps',function(req,res) {
 		} else {
 			res.status(500).end(req.query.id + ' not found!');
 		}
-	} else {
-		res.status(500).end('id missing!');
-	}
+	} 
 			});
 		});
 	}
