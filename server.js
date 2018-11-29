48ac2444042e79bd7c089140afa482206df2b4be
0903chris <42923822+0903chris@users.noreply.github.com>
2018-11-29 15:58:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -97,5 +97,34 @@ app.post('/create',function(req,res) {
 		});
 res.redirect('/');
 });
+app.get('/showdetails', function(req,res) {
+	console.log(req.session);
+	if (!req.session.authenticated) {
+		res.redirect('/login');
+	} 
+	else {
+		MongoClient.connect(mongourl, function(err, db) {
+		assert.equal(err,null);
+        	db.collection("restaurant").find().toArray(function(err,items){
+		var item = null;
+		if (req.query.id) {
+		for (i in items) {
+			if (items[i]._id == req.query.id) {
+				item = items[i]
+				break;
+			}
+		}
+		if (item) {
+			res.render('details', {r: items[i]});							
+		} else {
+			res.status(500).end(req.query.id + ' not found!');
+		}
+	} else {
+		res.status(500).end('id missing!');
+	}
+			});
+		});
+	}
+});
 
 app.listen(process.env.PORT || 8099);
