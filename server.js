740d1c741afc85bea8ecd8f544c1d72f65e2b745
0903chris <42923822+0903chris@users.noreply.github.com>
2018-11-30 17:34:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ function create(db,bfile,callback) {
   console.log(bfile);
   db.collection('photo').insertOne({
     "photo" : new Buffer(bfile.data).toString('base64'),
-    "photo mimetype" : bfile.mimetype
+    "photo mimetype" : bfile.mimetype,
 	"name":req.body.name,
 			"borough":req.body.borough,
 			"cuisine":req.body.cuisine,
