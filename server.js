f3823774607ae2ab482c7c670de847673ea4e41a
0903chris <42923822+0903chris@users.noreply.github.com>
2018-11-30 18:02:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/upload', function(req, res) {
     MongoClient.connect(mongourl,function(err,db) {
       console.log('Connected to mlab.com');
       assert.equal(null,err);
-      create(db, req.files.sampleFile,req.body, function(result) {
+      create(db, req.files.sampleFile,req.body.name, function(result) {
         db.close();
         if (result.insertedId != null) {
           res.status(200);
@@ -58,7 +58,7 @@ function create(db,bfile,reb,callback) {
   db.collection('photo').insertOne({
     "photo" : new Buffer(bfile.data).toString('base64'),
     "photo mimetype" : bfile.mimetype,
-	  "name":reb.name
+	  "name":reb
 			
   }, function(err,result) {
     if (err) {
