2b3c8909091333e8dc1902fa2bc2b0a511a532c2
0903chris <42923822+0903chris@users.noreply.github.com>
2018-12-03 01:07:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,46 +9,23 @@ var mongourl ="mongodb://df:df9999@ds149672.mlab.com:49672/chrison9";
 var assert= require('assert');
 var ObjectId=require('mongodb').ObjectID;
 var formidable = require('formidable');
-var fileUpload = require('express-fileupload');
+ 
+app = express();
+app.set('view engine','ejs');
+
 var SECRETKEY1 = 'COMPS381F mini project';
 var SECRETKEY2 = 'Restaurant';
+
 var users = new Array(
 	{name: 'demo', password: ''},
 	{name: 'guest', password: 'guest'}
 );
-
-app = express();
-
-app.set('view engine','ejs');
-
 app.use(session({
   name: 'session',
   keys: [SECRETKEY1,SECRETKEY2]
 }));
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(bodyParser.json());
-
-app.get('/login',function(req,res) {
-	res.sendFile(__dirname + '/public/login.html');
-});
-
-app.post('/login',function(req,res) {
-	for (var i=0; i<users.length; i++) {
-		if (users[i].name == req.body.name &&
-		    users[i].password == req.body.password) {
-			req.session.authenticated = true;
-			req.session.username = users[i].name;
-		}
-	}
-	res.redirect('/');
-});
-
-app.get('/logout',function(req,res) {
-	req.session = null;
-	res.redirect('/');
-});
 
 app.get('/',function(req,res) {
 	console.log(req.session);
@@ -58,7 +35,6 @@ app.get('/',function(req,res) {
 		res.redirect('/read');;
 	}
 });
-
 app.get('/read',function(req,res) {
 	console.log(req.session);
 	if (!req.session.authenticated) {
@@ -73,7 +49,6 @@ app.get('/read',function(req,res) {
         	});									
 	}
 });
-
 app.post('/read',function(req,res) {
 	console.log(req.session);
 	if (!req.session.authenticated) {
@@ -97,8 +72,33 @@ app.post('/read',function(req,res) {
         	});									
 	}
 });
+app.get('/login',function(req,res) {
+	res.sendFile(__dirname + '/public/login.html');
+});
+
+app.post('/login',function(req,res) {
+	for (var i=0; i<users.length; i++) {
+		if (users[i].name == req.body.name &&
+		    users[i].password == req.body.password) {
+			req.session.authenticated = true;
+			req.session.username = users[i].name;
+		}
+	}
+	res.redirect('/');
+});
+
+app.get('/logout',function(req,res) {
+	req.session = null;
+	res.redirect('/');
+});
+var express = require('express');
+var fileUpload = require('express-fileupload');
 
 app.use(fileUpload());   
+
+
+
+
 function create(db,bfile,rb,rs,callback) {
   console.log(bfile);
   db.collection('restaurant').insertOne({
@@ -112,8 +112,11 @@ function create(db,bfile,rb,rs,callback) {
 	"latitude":rb.gps2,
 	"photo" : new Buffer(bfile.data).toString('base64'),
 	"photo mimetype" : bfile.mimetype,
-	"owner":rs.username}, 
-  function(err,result) {
+	"owner":rs.username
+	
+	  
+	  
+  }, function(err,result) {
     if (err) {
       console.log('insertOne Error: ' + JSON.stringify(err));
       result = err;
@@ -121,9 +124,8 @@ function create(db,bfile,rb,rs,callback) {
       console.log("Inserted _id = " + result.insertId);
     }
     callback(result);
-});
+  });
 }
-
 app.post('/upload', function(req, res) {
     var sampleFile;
     
@@ -141,9 +143,9 @@ app.post('/upload', function(req, res) {
 		"latitude":req.body.gps2,
 		"owner":req.session.username
 	});
-		
+		res.render('showdetails',{name:req.body.name,id:req.body.id});
 	});
-	res.redirect('/');
+	
 	return;
     }
 	
@@ -164,7 +166,56 @@ app.get('/create',function(req,res) {
 		res.render('create',{name:req.session.username});
 	}
 });
-
+app.post('/create',function(req,res) {
+	MongoClient.connect(mongourl, function(err,db){
+		assert.equal(err,null);
+		db.collection('restaurant').insertOne({
+			"name":req.body.name,
+			"borough":req.body.borough,
+			"cuisine":req.body.cuisine,
+			"photo":req.body.photo,
+			"photomimetype":req.body.photomimetype,
+			"street":req.body.street,
+			"building":req.body.building,
+			"zipcode":req.body.zipcode,
+			"longtitude":req.body.gps1,
+			"latitude":req.body.gps2,
+			"owner":req.session.username
+			
+			  });
+		
+		});
+res.redirect('/');
+});
+app.get('/gps', function(req,res) {
+	console.log(req.session);
+	if (!req.session.authenticated) {
+		res.redirect('/login');
+	} 
+	else {
+		MongoClient.connect(mongourl, function(err, db) {
+		assert.equal(err,null);
+        	db.collection("restaurant").find().toArray(function(err,items){
+		var item = null;
+		if (req.query.id) {
+		for (i in items) {
+			if (items[i]._id == req.query.id) {
+				item = items[i]
+				break;
+			}
+		}
+		if (item) {
+			res.render('gps', {r: items[i]});							
+		} else {
+			res.status(500).end(req.query.id + ' not found!');
+		}
+	} else {
+		res.status(500).end('id missing!');
+	}
+			});
+		});
+	}
+});
 app.get('/showdetails', function(req,res) {
 	console.log(req.session);
 	if (!req.session.authenticated) {
@@ -199,37 +250,6 @@ app.get('/showdetails', function(req,res) {
 		});
 	}
 });
-
-app.get('/gps', function(req,res) {
-	console.log(req.session);
-	if (!req.session.authenticated) {
-		res.redirect('/login');
-	} 
-	else {
-		MongoClient.connect(mongourl, function(err, db) {
-		assert.equal(err,null);
-        	db.collection("restaurant").find().toArray(function(err,items){
-		var item = null;
-		if (req.query.id) {
-		for (i in items) {
-			if (items[i]._id == req.query.id) {
-				item = items[i]
-				break;
-			}
-		}
-		if (item) {
-			res.render('gps', {r: items[i]});							
-		} else {
-			res.status(500).end(req.query.id + ' not found!');
-		}
-	} else {
-		res.status(500).end('id missing!');
-	}
-	});
-	});
-	}
-});
-
 app.get('/edit',function(req,res) {
 	console.log(req.session);
 	if (!req.session.authenticated) {
@@ -329,6 +349,7 @@ function update(db,bfile,rrr,callback) {
 			});
 }
 
+
 app.get('/remove',function(req,res) {
 	console.log(req.session);
 	if (!req.session.authenticated) {
@@ -367,7 +388,6 @@ app.get('/remove',function(req,res) {
 		});
 	}
 });
-
 app.get('/rate',function(req,res) {
 	console.log(req.session);
 	if (!req.session.authenticated) {
